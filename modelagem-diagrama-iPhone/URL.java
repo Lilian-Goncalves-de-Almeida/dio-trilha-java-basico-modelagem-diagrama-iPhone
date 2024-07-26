46ef8a7cfb17a77dabf155aefd7522d365854384
Lilian Gonçalves de Almeida <59492113+Lilian-Goncalves-de-Almeida@users.noreply.github.com>
2024-07-26 12:47:51 -0300

    Update URL.java 1

--- modelagem-diagrama-iPhone/URL.java
+++ modelagem-diagrama-iPhone/URL.java
@@ -2,6 +2,15 @@ public class URL {
 
     private String link;
 
+    public void setLink(String link) {
+        this.link = link;
+    }
+
+    public String getLink() {
+        return link;
+    }
+
+
     public void chamarURL(){
         System.out.println(String.format("chamar URL: %s",this.link));
     }
